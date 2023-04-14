f9901fe80c3450a96fca9c373ddfa615ea5b4ff1
Johannes Finne Reinseth <johannes.reinseth@aboveit.no>
2023-04-14 14:14:41 +0200

    fix: GSM

--- core/src/com/mygdx/game/HeroesVsMonsters.java
+++ core/src/com/mygdx/game/HeroesVsMonsters.java
@@ -1,41 +1,43 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.states.DummyState;
+import com.mygdx.game.states.GameStateManager;
 
 public class HeroesVsMonsters extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	FireBaseInterface FBIC;
-
+	SpriteBatch sb;
+	GameStateManager gsm;
+	//FireBaseInterface FBIC;
 
 	public HeroesVsMonsters(FireBaseInterface FBIC) {
-		this.FBIC = FBIC;
+		//this.FBIC = FBIC;
 	}
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		FBIC.SomeFunction();
+		sb = new SpriteBatch();
+		/*FBIC.SomeFunction();
 		FBIC.FirstFirebaseTest();
 		FBIC.SetOnValueChangedListener();
 		FBIC.SetValueInDb("message", "Updated message!");
+		 */
+		gsm = GameStateManager.getGsm();
+		gsm.push(new DummyState());
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(sb);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		sb.dispose();
 	}
 }
