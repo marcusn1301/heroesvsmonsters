e5e0a8e6a583b166c60817e945749d303476c17b
Anh <qapham@stud.ntnu.no>
2023-04-14 10:14:51 +0200

    Add start and lobby button to game menu

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -4,13 +4,12 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.HeroesVsMonsters;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new HeroesVsMonsters(new AndroidInterfaceClass()), config);
+		initialize(new GameMenu(new AndroidInterfaceClass()), config);
 	}
 }
