bd6434c928765b24a7b1557fb0626355dcffd32e
sindrimt <sindrimt@gmail.com>
2023-04-23 12:13:46 +0200

    Updated FirebaseInterface method

--- core/src/com/mygdx/game/FireBaseInterface.java
+++ core/src/com/mygdx/game/FireBaseInterface.java
@@ -3,6 +3,6 @@ package com.mygdx.game;
 public interface FireBaseInterface {
     public void SomeFunction();
     public void FirstFirebaseTest();
-    public void SetOnValueChangedListener();
+    public void SetOnValueChangedListener(String target);
     public void SetValueInDb(String target, Integer value);
 }
